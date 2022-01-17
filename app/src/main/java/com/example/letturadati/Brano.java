5579d6ddaeae0066f0f4fa1be1419e893649cf3f
aletaffo <alessandrotaffini@gmail.com>
2022-01-17 20:41:38 +0100

    secondo punto

--- app/src/main/java/com/example/letturadati/Brano.java
+++ app/src/main/java/com/example/letturadati/Brano.java
@@ -3,14 +3,51 @@
     import java.util.Date;
 
     public class Brano {
+
         private String titolo;
         private String autore;
         private String genere;
-        private String urlVideo;
         private int durata;
+        private String urlVideo;
         private Date dataLancio;
 
+            public Brano(String titolo, String autore, String genere, int durata){
+                this.titolo = titolo;
+                this.autore = autore;
+                this.genere = genere;
+                this.durata = durata;
+            }
 
-
+            public Brano(String titolo){
+              this.titolo = titolo;
+             }
+//Titolo
+        public String getTitolo() {
+            return titolo;
+        }
+        public void setTitolo(String titolo) {
+            this.titolo = titolo;
+        }
+//Autore
+        public String getAutore() {
+            return autore;
+        }
+        public void setAutore(String autore) {
+            this.autore = autore;
+        }
+//Genere
+        public String getGenere() {
+            return genere;
+        }
+        public void setGenere(String genere) {
+            this.genere = genere;
+        }
+//Durata
+        public int getDurata() {
+            return durata;
+        }
+        public void setDurata(int durata) {
+            this.durata = durata;
+        }
 
 }
