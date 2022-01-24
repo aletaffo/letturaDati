8791cadc0fb11dbd452638ebbed3590522e79736
aletaffo <alessandrotaffini@gmail.com>
2022-01-24 16:58:54 +0100

    quarto activity

---
+++ app/src/main/java/com/example/letturadati/Visualizza_Activity.java
@@ -0,0 +1,15 @@
+package com.example.letturadati;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Visualizza_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_visualizza);
+
+    }
+}
